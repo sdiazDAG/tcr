aaccb9455d7c29ea38111db05252538605a914d0
Santi Diaz <sdiaz@aidacanarias.com>
2022-04-01 12:13:06 +0100

    working

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -8,7 +8,18 @@ namespace tcr
         [Fact]
         public void Test1()
         {
-
+          int posotion = 1;
+          int resultValue = Fibonacci.Calculate(posotion);
+          int expectedValue = 0;
+          Assert.Equal(expectedValue, resultValue);
         }
+    }
+
+    public static class Fibonacci
+    {
+      public static int Calculate(int posotion)
+      {
+        return 0;
+      }
     }
 }
